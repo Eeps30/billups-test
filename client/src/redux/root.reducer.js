4dcab15ed48053df8b15af5e6b82dd2018f9381d
Eeps30 <eeps30@gmail.com>
2019-09-15 21:26:27 -0700

    created instructions for readme, removed reducers that were no longer in use, changed npm script command for installing dependencies

--- client/src/redux/root.reducer.js
+++ client/src/redux/root.reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import incrementReducer from './increment/increment.reducer';
 import choiceReducer from './choices/choice.reducer';
 
 const rootReducer = combineReducers({
-  increment: incrementReducer,
   choice: choiceReducer
 });
 
